13fb769bf50c960f29edc267845096dea055b39c
kunal <kunalrai67890@gmail.com>
2022-12-12 14:54:44 +0530

    Add discription to the new columns

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ with col1:
 with col2:
     st.title("Kunal Rai")
     content = """ Hi, I am Kunal! I am a Python programmer, Ethical Hacker, teacher, and founder of GreyCode.
-    I love to write code and check system security 
+    I love to write code and check system security , I hava build web app , security app and automation .
     """
-    st.write(content)
+    st.info(content)
+
+
+content2 = """ 
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+
+st.write(content2)
