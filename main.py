95af6b4225705d653fae477fec1263efcf8a2023
kunal <kunalrai67890@gmail.com>
2022-12-14 08:54:29 +0530

    Render the title

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import streamlit as st
+import pandas
+
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo2.jpg")
 
 with col2:
     st.title("Kunal Rai")
-    content = """ Hi, I am Kunal! I am a Python programmer, Ethical Hacker, teacher, and founder of GreyCode.
-    I love to write code and check system security , I hava build web app , security app and automation .
+    content = """ Hi, I am Kunal! I am a Python programmer, Ethical Hacker, 
+    teacher, and founder of GreyCode.
+    I love to write code and check system security , 
+    I hava build web app , security app and automation script .
     """
     st.info(content)
-
 
 content2 = """ 
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
